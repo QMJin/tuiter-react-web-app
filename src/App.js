9a918971e9b7879fe11ba3e4ce768b38b7e8160e
qmjin <jin.qimi@northeastern.edu>
2022-09-16 14:56:02 -0700

    add branch

--- src/App.js
+++ src/App.js
@@ -13,14 +13,6 @@ function App() {
           Welcome to Web Development Fall 2022
         </h1>
         <h2> This is the branch for assignment 2</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </h2h1a>
       </header>
     </div>
   );
