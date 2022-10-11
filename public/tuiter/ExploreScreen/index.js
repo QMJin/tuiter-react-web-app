618fbc8d92b2c298a7697107b6a182489208cf47
qmjin <jin.qimi@northeastern.edu>
2022-10-11 10:15:48 -0700

    test2

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -11,6 +11,9 @@ function exploreComponent() {
         <div class="col-10 col-lg-7 col-xl-6">
           ${ExploreComponent()}
         </div>
+        <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+          ${WhoToFollowList()}
+        </div>
       </div>
   `)
 }
