6b1ab7a94e6eb93e183a9779bdf686f761e57e23
qmjin <jin.qimi@northeastern.edu>
2022-10-11 10:18:50 -0700

    test3

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -5,9 +5,7 @@ import WhoToFollowList from "../WhoToFollowList/index.js";
 function exploreComponent() {
   $('#wd-explore').append(`
       <div class="row mt-2">
-        <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-          ${NavigationSidebar()}
-        </div>
+
         <div class="col-10 col-lg-7 col-xl-6">
           ${ExploreComponent()}
         </div>
