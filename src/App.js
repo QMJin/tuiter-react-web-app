a8dc20e2c062e41611c0425449259d3d6924bef0
qmjin <jin.qimi@northeastern.edu>
2022-11-29 19:06:39 -0800

    assignment 9

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
-import Tuiter from "./services";
+import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes,Route} from "react-router";
 /*import Assignment7 from "./labs/a7";*/
@@ -13,7 +13,7 @@ function App() {
            {/* <Route index element={<Labs/>}/>*/}
            {/*<Route path="/a7" element={<Assignment7/>}/>*/}
             <Route index element={<Tuiter/>}/>
-            {/*<Route path="/tuiter/*" element={<Tuiter/>}/>*/}
+            <Route path="/tuiter/*" element={<Tuiter/>}/>
           </Routes>
         </div>
       </BrowserRouter>
